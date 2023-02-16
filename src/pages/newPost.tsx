2592da629f0e37691b071adfe22a943adf082df0
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2023-02-16 11:08:20 +0100

    design on add new post

--- src/pages/newPost.tsx
+++ src/pages/newPost.tsx
@@ -6,31 +6,35 @@ export default function addNewPost() {
             <div className="header" id="header">
                 <img src="/images/stuns_logo.png" alt="logo" />
             </div>
-            <div className="addNewPost">
+            <div className="addPostContainer">
                 <div className="addNewPostContainer">
                     <div className="addNewPostTitle">
-                        <h1>Add New Post</h1>
+                        <h1>Lägg till ett inlägg</h1>
                     </div>
                     <div className="addNewPostForm">
                         <form>
-                            <div className="addNewPostFormTitle">
-                                <label htmlFor="title">Title</label>
+                            <div className="addNewPostFormName">
+                                <label htmlFor="title">Namn</label>
+                                <input type="text" id="title" name="title" />
+                            </div>
+                            <div className="addNewPostFormOrganization">
+                                <label htmlFor="title">Organisation</label>
                                 <input type="text" id="title" name="title" />
                             </div>
                             <div className="addNewPostFormDescription">
-                                <label htmlFor="description">Description</label>
+                                <label htmlFor="description">Beskrivning</label>
                                 <textarea id="description" name="description" rows={4} cols={50} />
                             </div>
                             <div className="addNewPostFormImage">
-                                <label htmlFor="image">Image</label>
+                                <label htmlFor="image">Bild</label>
                                 <input type="file" id="image" name="image" />
                             </div>
                             <div className="addNewPostFormLocation">
-                                <label htmlFor="location">Location</label>
-                                <input type="text" id="location" name="location" />
+                                <label htmlFor="location">Plats</label>
+                                <input type="text" id="location" name="location" placeholder="Skriv dina koordinater" />
                             </div>
                             <div className="addNewPostFormSubmit">
-                                <button type="submit">Submit</button>
+                                <button type="submit">Spara</button>
                             </div>
                         </form>
                     </div>
