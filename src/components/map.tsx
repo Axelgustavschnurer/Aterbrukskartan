b36c8bccf7b7eaa685d66442a116badd6c884fba
ahedenskog <albin.hedenskog@elev.ga.ntig.se>
2023-02-16 11:24:17 +0100

    More pin functionality

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -3,6 +3,12 @@ import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 
 const Map = () => {
+    const pins = [ // This is an array of objects with the coordinates of the pins
+    { lat: 51.505, lng: -0.09 },
+    { lat: 51.51, lng: -0.1 },
+    { lat: 51.51, lng: -0.12 },
+    ]
+
     const icon = new L.Icon({
         iconUrl: 'https://www.galeo.com.mx/wp-content/uploads/2016/06/map-marker-icon.png',
         iconRetinaUrl: '/marker-icon-2x.png',
@@ -10,6 +16,19 @@ const Map = () => {
         iconSize: [25, 25],
     });
 
+    const getAllPins = () => {
+        return pins.map((pin, i) => {
+            return (
+                <Marker key={i} position={[pin.lat, pin.lng]} icon={icon}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            )
+        }
+        )
+    }
+    console.log(getAllPins())
     return (
         <>
             <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100%" }} zoomControl={false}>
@@ -18,11 +37,7 @@ const Map = () => {
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]} icon={icon}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {getAllPins()}
             </MapContainer>
         </>
     )
