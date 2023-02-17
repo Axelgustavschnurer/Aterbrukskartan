22a0982a8d369c1d8c7520b974a43dfd5d279395
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2023-02-17 11:58:44 +0100

    asdada

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,13 +1,29 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 export default function sidebar() {
+    const router = useRouter();
+
+    const goToSomewhere = () => {
+        router.push("/api/hello");
+    };
+
     return (
         <>
             <div className="sidebar">
                 <div className="filterBtn">
-                    <button id="demoBtn"><img src="/images/riv.png" width={60} height={60}></img></button>
-                    <button id="buildBtn"><img src="/images/bygg.png" width={60} height={60}></img></button>
-                    <button id="rebuildBtn"><img src="/images/ater.png" width={60} height={60}></img></button>
+                    <div className="alignBtn">
+                        <button id="demoBtn" onClick={goToSomewhere}><img src="/images/riv.svg"></img></button>
+                        <p>Riv</p>
+                    </div>
+                    <div className="alignBtn">
+                        <button id="buildBtn"><img src="/images/bygg.svg" ></img></button>
+                        <p>Bygg</p>
+                    </div>
+                    <div className="alignBtn">
+                        <button id="rebuildBtn"><img src="/images/ater.svg"></img></button>
+                        <p>Återbygg</p>
+                    </div>
                 </div>
             </div>
         </>
