401e8b673f3790e23356d9e9c9f032ec0e1b4987
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2023-02-20 14:09:59 +0100

    can hide sidebar onclick

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 export default function sidebar({ setFilter }: any) {
+
+    const [isOpen, setOpen] = useState(true);
+    const toggleMenu = () => setOpen(!isOpen);
+
+    const router = useRouter();
+
+    useEffect(() => {
+        const closeMenu = () => isOpen && setOpen(false);
+        router.events.on("routeChangeStart", closeMenu);
+        return () => {
+            router.events.off("routeChangeStart", closeMenu);
+        };
+    }, [isOpen, router]);
 
     const onButtonClick = (filter: any) => {
         setFilter(filter);
@@ -10,25 +24,37 @@ export default function sidebar({ setFilter }: any) {
 
     return (
         <>
-            <div className="sidebar">
-                <div className="filterBtn">
-                    <div className="alignBtn">
-                        <button id="demoBtn" onClick={() => onButtonClick("rivning")}><img src="/images/riv.svg"></img></button>
-                        <p>Riv</p>
+            {isOpen && (
+                <div className="sidebar">
+                    <div className="filterBtn">
+                        <div className="alignBtn">
+                            <button id="demoBtn" onClick={() => onButtonClick("rivning")}><img src="/images/riv.svg"></img></button>
+                            <p>Riv</p>
+                        </div>
+                        <div className="alignBtn">
+                            <button id="buildBtn" onClick={() => onButtonClick("byggnad")}><img src="/images/bygg.svg" ></img></button>
+                            <p>Bygg</p>
+                        </div>
+                        <div className="alignBtn">
+                            <button id="rebuildBtn" onClick={() => onButtonClick("ombyggnad")}><img src="/images/ater.svg"></img></button>
+                            <p>Återbygg</p>
+                        </div>
                     </div>
-                    <div className="alignBtn">
-                        <button id="buildBtn" onClick={() => onButtonClick("byggnad")}><img src="/images/bygg.svg" ></img></button>
-                        <p>Bygg</p>
+                    <div className="clearFilter">
+                        <button id="clearBtn" onClick={() => onButtonClick("none")}>Rensa filter</button>
                     </div>
-                    <div className="alignBtn">
-                        <button id="rebuildBtn" onClick={() => onButtonClick("ombyggnad")}><img src="/images/ater.svg"></img></button>
-                        <p>Återbygg</p>
+                    <div className="sidebarClose">
+                        <button id="hideBtn" onClick={toggleMenu}><img src="/closeArrow.svg" alt="Closing arrow" /></button>
                     </div>
                 </div>
-                <div className="clearFilter">
-                    <button id="clearBtn" onClick={() => onButtonClick("none")}>Rensa filter</button>
+            )}
+            {!isOpen && (
+                <div className="hiddenSidebar">
+                    <div className="sidebarOpen">
+                        <button id="openBtn" onClick={toggleMenu}><img src="/openArrow.svg" alt="Open arrow" /></button>
+                    </div>
                 </div>
-            </div>
+            )}
         </>
     );
 }
