6316969e5c90e49baa68a3d35dd718d810c64831
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2023-02-27 09:52:28 +0100

    new markers

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,21 +1,21 @@
 import L from 'leaflet';
 
 const IconPinRed = new L.Icon({
-    iconUrl: ('/images/redicon.png'),
+    iconUrl: ('/images/redicon.svg'),
     iconRetinaUrl: ('/images/redicon.png'),
-    iconSize: new L.Point(30, 30),
+    iconSize: new L.Point(25, 25),
 });
 
 const IconPinGreen = new L.Icon({
-    iconUrl: ('/images/greenicon.png'),
+    iconUrl: ('/images/greenicon.svg'),
     iconRetinaUrl: ('/images/greenicon.png'),
-    iconSize: new L.Point(20, 30),
+    iconSize: new L.Point(25, 25),
 });
 
 const IconPinBlue = new L.Icon({
-    iconUrl: ('/images/blueicon.png'),
+    iconUrl: ('/images/blueicon.svg'),
     iconRetinaUrl: ('/images/blueicon.png'),
-    iconSize: new L.Point(20, 30),
+    iconSize: new L.Point(25, 25),
 });
 
 export { IconPinRed, IconPinGreen, IconPinBlue };
