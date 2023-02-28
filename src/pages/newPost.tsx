adca930ae94f5dcdaf5cdda3d908f1b7115107e9
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2023-02-28 15:47:59 +0100

    Start on post requests to db

--- src/pages/newPost.tsx
+++ src/pages/newPost.tsx
@@ -3,13 +3,69 @@ import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 import Head from "next/head";
+import { latLng } from "leaflet";
+
 
 export default function AddNewPost() {
+    const [organization, setOrganization] = useState("");
+    const [projectType, setProjectType] = useState("");
+    const [location, setLocation] = useState("");
+    const [searchingFor, setSearchingFor] = useState("");
+    const [offering, setOffering] = useState("");
+    const [description, setDescription] = useState("");
+    const [contact, setContact] = useState("");
+    const [message, setMessage] = useState("");
+
     const router = useRouter();
 
-    const handleSubmit = (e: any) => {
+    const getProjectType = (e: any) => {
+        setProjectType(e.target.value);
+        console.log("value: ", e.target.value);
+    };
+
+    const handleSubmit = async (e: any) => {
         e.preventDefault();
-        router.push("/");
+        try {
+            console.log(JSON.stringify({
+                organization,
+                projectType,
+                location,
+                searchingFor,
+                offering,
+                description,
+                contact,
+            }))
+            let res = await fetch("http://localhost:3000/api/posts", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    organization,
+                    projectType,
+                    location,
+                    searchingFor,
+                    offering,
+                    description,
+                    contact,
+                }),
+            });
+            let resJson = await res.json();
+            if (res.status === 200) {
+                setOrganization("");
+                setProjectType("");
+                setLocation("");
+                setSearchingFor("");
+                setOffering("");
+                setDescription("");
+                setContact("");
+                router.push("/");
+            } else {
+                setMessage("Something went wrong")
+            }
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     const NewPostMap = React.useMemo(() => dynamic(
@@ -38,20 +94,45 @@ export default function AddNewPost() {
                         <form method="post" onSubmit={handleSubmit}>
                             <div className="addNewPostFormOrganization">
                                 <label className="newPostTitle" htmlFor="organization">* Organisation</label>
-                                <input type="text" id="organization" name="organization" required />
+                                <input
+                                    type="text"
+                                    id="organization"
+                                    name="organization"
+                                    value={organization}
+                                    onChange={(e) => setOrganization(e.target.value)}
+                                    required
+                                />
                             </div>
                             <div className="typeOfProject">
                                 <label className="newPostTitle" htmlFor="type">Typ av projekt</label>
                                 <div className="test">
-                                    <input type="radio" id="riv" name="category" value="riv" />
+                                    <input
+                                        type="radio"
+                                        id="riv"
+                                        name="category"
+                                        value="riv"
+                                        onChange={getProjectType}
+                                    />
                                     <label htmlFor="riv">Rivning </label>
                                 </div>
                                 <div className="test">
-                                    <input type="radio" id="bygg" name="category" value="bygg" />
+                                    <input
+                                        type="radio"
+                                        id="bygg"
+                                        name="category"
+                                        value="bygg"
+                                        onChange={getProjectType}
+                                    />
                                     <label htmlFor="bygg">Byggnation </label>
                                 </div>
                                 <div className="test">
-                                    <input type="radio" id="ombygge" name="category" value="ombygge" />
+                                    <input
+                                        type="radio"
+                                        id="ombygge"
+                                        name="category"
+                                        value="ombygge"
+                                        onChange={getProjectType}
+                                    />
                                     <label htmlFor="ombygge">Ombyggnation</label>
                                 </div>
                             </div>
@@ -106,15 +187,33 @@ export default function AddNewPost() {
                             </div>
                             <div className="addNewPostFormDescription">
                                 <label className="newPostTitle" htmlFor="description">* Beskrivning</label>
-                                <textarea id="description" name="description" rows={10} maxLength={3000} required placeholder="Hur mycket (Ex. mått och vikt) och kort om skicket på produkten. " />
+                                <textarea
+                                    id="description"
+                                    name="description"
+                                    rows={10}
+                                    maxLength={3000}
+                                    placeholder="Hur mycket (Ex. mått och vikt) och kort om skicket på produkten."
+                                    value={description}
+                                    onChange={(e) => setDescription(e.target.value)}
+                                    required
+                                />
                             </div>
                             <div className="addNewPostFormContact">
                                 <label className="newPostTitle" htmlFor="contact">* Kontakt</label>
-                                <textarea id="contact" name="contact" rows={3} cols={100} required />
+                                <textarea
+                                    id="contact"
+                                    name="contact"
+                                    rows={3}
+                                    cols={100}
+                                    value={contact}
+                                    onChange={(e) => setContact(e.target.value)}
+                                    required
+                                />
                             </div>
                             <div className="addNewPostFormSubmit">
                                 <button type="submit">Spara</button>
                             </div>
+                            <div className="message">{message ? <p>{message}</p> : null}</div>
                         </form>
                     </div>
                 </div>
