5aa3cf2579dddc08a273c46bc2379d05b1be38a9
Leon-Loov <leon.loov@elev.ga.ntig.se>
2023-03-01 15:40:41 +0100

    Add filtering by lookingForMaterials

--- src/functions/filterData.tsx
+++ src/functions/filterData.tsx
@@ -1,7 +1,7 @@
 import { DeepRecycle, Filter } from "@/types";
 
 // export default runActiveFilters;
-export { filterByProjectType, filterByYear };
+export { filterByProjectType, filterByYear, filterByLookingFor };
 
 function filterByProjectType(data: DeepRecycle[], projectType: string[]): DeepRecycle[] {
   let returnData: DeepRecycle[] = [];
@@ -30,24 +30,30 @@ function filterByYear(data: DeepRecycle[], year: number[]): DeepRecycle[] {
   return returnData;
 }
 
-/*function filterByLookingFor(data: DeepRecycle[], lookingFor: string[]): DeepRecycle[] {
+function filterByLookingFor(data: DeepRecycle[], lookingFor: string[]): DeepRecycle[] {
   let returnData: DeepRecycle[] = [];
+  // Loop through the list of all map tacks
   for (let i in data) {
-    switch (data[i].lookingForMaterials) {
-      case lookingFor[0]:
-      case lookingFor[1]:
-      case lookingFor[2]:
-      case lookingFor[3]:
-        returnData.push(data[i]);
-        continue;
-        break;
+    // The leaveInnerLoop label and ´break leaveInnerLoop;´ are used to break out of the for loop from within a switch statement
+    leaveInnerLoop: {
+      // Split the lookingForMaterials string and compare each material within to the materials in the filter.
+      // If *ANY* material in the string matches *ANY* material in the filter, the map tack is added to the returnData array and thus not filtered away.
+      for (let j in data[i].lookingForMaterials?.split(", ")) {
+        switch (j) {
+          case lookingFor[0]:
+          case lookingFor[1]:
+          case lookingFor[2]:
+          case lookingFor[3]:
+            returnData.push(data[i]);
+            break leaveInnerLoop;
 
-      default:
-        break;
+          default:
+            break;
+        }
+      }
     }
-    break;
   }
   return returnData;
-}*/
+}
 
 // function runActiveFilters(data: DeepRecycle[], filters: Filter[]): DeepRecycle[];
