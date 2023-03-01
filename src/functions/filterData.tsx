bd1427dc7b4a201139c45d0107bc185d94033821
Leon-Loov <leon.loov@elev.ga.ntig.se>
2023-03-01 15:22:08 +0100

    Add filte for year

--- src/functions/filterData.tsx
+++ src/functions/filterData.tsx
@@ -1,20 +1,53 @@
 import { DeepRecycle, Filter } from "@/types";
 
-export default runActiveFilters;
-
-
-function runActiveFilters(data: DeepRecycle[], activeFilters: Filter): DeepRecycle[];
-
-function filterByProjectType(data: DeepRecycle[], projectType: string[]): DeepRecycle[];
-
+// export default runActiveFilters;
+export { filterByProjectType, filterByYear };
 
 function filterByProjectType(data: DeepRecycle[], projectType: string[]): DeepRecycle[] {
   let returnData: DeepRecycle[] = [];
-  for ( let i in data ){
-    // TODO: Refactor this to be better
-    if (data[i].projectType === projectType[0] || data[i].projectType === projectType[1] || data[i].projectType === projectType[2]) {
+  for (let i in data) {
+    switch (data[i].projectType) {
+      case projectType[0]:
+      case projectType[1]:
+      case projectType[2]:
+        returnData.push(data[i]);
+        break;
+
+      default:
+        break;
+    }
+  }
+  return returnData;
+}
+
+function filterByYear(data: DeepRecycle[], year: number[]): DeepRecycle[] {
+  let returnData: DeepRecycle[] = [];
+  for (let i in data) {
+    if (data[i].mapItem.year! <= Math.max(...year) && data[i].mapItem.year! >= Math.min(...year)) {
       returnData.push(data[i]);
     }
   }
   return returnData;
 }
+
+/*function filterByLookingFor(data: DeepRecycle[], lookingFor: string[]): DeepRecycle[] {
+  let returnData: DeepRecycle[] = [];
+  for (let i in data) {
+    switch (data[i].lookingForMaterials) {
+      case lookingFor[0]:
+      case lookingFor[1]:
+      case lookingFor[2]:
+      case lookingFor[3]:
+        returnData.push(data[i]);
+        continue;
+        break;
+
+      default:
+        break;
+    }
+    break;
+  }
+  return returnData;
+}*/
+
+// function runActiveFilters(data: DeepRecycle[], filters: Filter[]): DeepRecycle[];
