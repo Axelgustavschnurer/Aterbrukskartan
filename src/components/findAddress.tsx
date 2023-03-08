4867c497aed5d5c3bf4bd0aa0ab989953e434fab
villegust <vilhelm.gustavsson@elev.ga.ntig.se>
2023-03-08 10:56:29 +0100

    Added search function to search for streetaddresses and toggle button to toggle toold coordinatesystem

--- src/components/findAddress.tsx
+++ src/components/findAddress.tsx
@@ -1,15 +1,16 @@
 import { useState } from 'react';
+import dynamic from 'next/dynamic';
 
-export const LeafletAddressLookup = () => {
-    const [lat, setLat] = useState(40.75637123);
-    const [lon, setLon] = useState(-73.98545321);
+const LeafletAddressLookup = () => {
+    const [lat, setLat] = useState();
+    const [lon, setLon] = useState();
     const [location, setLocation] = useState('');
     const [results, setResults] = useState([]);
 
     const handleAddressClick = (lat: any, lon: any) => {
-        let latitude: any = parseFloat(lat).toFixed(8);
+        let latitude: any = parseFloat(lat).toFixed(6);
         setLat(latitude);
-        let longitude: any = parseFloat(lon).toFixed(8);
+        let longitude: any = parseFloat(lon).toFixed(6);
         setLon(longitude);
     };
 
@@ -27,38 +28,46 @@ export const LeafletAddressLookup = () => {
     };
 
     return (
-        <div className="container">
-            <b>Coordinates</b>
-            <form>
-                <input type="text" name="lat" id="lat" size={12} value={lat} onChange={(e) => setLat(Number(e.target.value))} />
-                <input type="text" name="lon" id="lon" size={12} value={lon} onChange={(e) => setLon(Number(e.target.value))} />
-            </form>
-
-            <b>Address Lookup</b>
-            <div id="search">
-                <input type="text" name="addr" value={location} id="addr" size={58} onChange={(e) => setLocation(e.target.value)} />
-                <button type="button" onClick={handleSearch}>
-                    Search
-                </button>
-                <div id="results">
-                    {results.length > 0 ? (
-                        results.map((result) => (
-                            <div
-                                key={result.place_id}
-                                className="address"
-                                title="Show Location and Coordinates"
-                                onClick={() => handleAddressClick(result.lat, result.lon)}
-                            >
-                                {result.display_name}
-                            </div>
-                        ))
-                    ) : (
-                        <div>Sorry, no results...</div>
-                    )}
+        <div className="findAddressContainer">
+            <b>Skriv adress</b>
+            <div className="addressContent">
+                <div id="search">
+                    <div id='searchDir'>
+                        <input type="text" name="addr" value={location} id="addr" onChange={(e) => setLocation(e.target.value)} />
+                        <button id="addressBtn" type="button" onClick={handleSearch}>
+                            Sök
+                        </button>
+                    </div>
+                    <div id="results">
+                        {results.length > 0 ? (
+                            results.map((result) => (
+                                <div
+                                    key={result.place_id}
+                                    className="address"
+                                    title="Show Location and Coordinates"
+                                    onClick={() => handleAddressClick(result.lat, result.lon)}
+                                >
+                                    {`• ${result.display_name}`}
+                                </div>
+                            ))
+                        ) : (
+                            <div>Tyvärr, adressen hittades inte</div>
+                        )}
+                    </div>
                 </div>
             </div>
+            <b>Koordinater</b>
+            <form>
+                <label htmlFor="lat" style={{ margin: "5px" }}>Latitud:</label>
+                <input type="text" name="lat" id="lat" placeholder='Latitud' value={lat} onChange={(e) => setLat(Number(e.target.value))} />
+                <label htmlFor="lon" style={{ margin: "5px" }}>Longitud:</label>
+                <input type="text" name="lon" id="lon" placeholder='Longitud' value={lon} onChange={(e) => setLon(Number(e.target.value))} />
+            </form>
+
 
             <br />
-        </div>
+        </div >
     );
 };
+
+export default dynamic(() => Promise.resolve(LeafletAddressLookup), { ssr: false })
