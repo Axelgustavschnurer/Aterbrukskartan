026366a838b79d48214f4aaa1305a899cc5c2652
Leon-Loov <leon.loov@elev.ga.ntig.se>
2023-03-10 11:07:47 +0100

    Add error handling and PUT functionality to editData

--- src/pages/api/editData.ts
+++ src/pages/api/editData.ts
@@ -16,6 +16,8 @@ export default async function handler(
         /**
          * Returns the `Recycle` object with the given ID, with the mapItem object included, or throws an error if no `Recycle` object with the given ID exists.
          */
+        if (!parseInt(req.query.id as string)) throw new Error('No ID specified');
+
         const getData: DeepRecycle = await prisma.recycle.findFirstOrThrow({
           where: {
             id: parseInt(req.query.id as string)
@@ -26,13 +28,70 @@ export default async function handler(
         })
 
         res.status(200).json(getData)
-      } catch (err) {
-        res.status(400).json({ message: 'Something went wrong, the requested ID might not exist in the database' });
+      }
+      catch (err: any) {
+        if (err.message === 'No ID specified') {
+          res.status(400).json({ message: 'No ID specified' })
+        }
+        else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+          res.status(400).json({ message: 'Something went wrong when processing the request. The specified ID might not exist in the database.' });
+        }
+        else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+          res.status(400).json({ message: 'Something went wrong when processing the request. Cause unknown.' });
+        }
+        else if (err instanceof Prisma.PrismaClientInitializationError) {
+          res.status(500).json({ message: 'Internal server error. Failed to connect to database.' });
+        }
+        else {
+          res.status(500).json({ message: 'Internal server error' });
+        }
       }
       break;
 
     case 'PUT':
-      res.status(501).json({ message: 'Not implemented' });
+      try {
+        if (!parseInt(req.query.id as string)) throw new Error('No ID specified');
+
+        const updateData: DeepRecycleInput = req.body;
+        const updatedData = await prisma.recycle.update({
+          where: {
+            id: parseInt(req.query.id as string)
+          },
+
+          data: {
+            ...updateData,
+            mapItem: {
+              update: { ...updateData.mapItem }
+            },
+          },
+
+          include: {
+            mapItem: true
+          },
+        });
+
+        res.status(200).json(updatedData);
+      }
+      catch (err: any) {
+        if (err.message === 'No ID specified') {
+          res.status(400).json({ message: 'No ID specified' })
+        }
+        else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+          res.status(400).json({ message: 'Something went wrong when processing the request. The specified ID might not exist in the database.' });
+        }
+        else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+          res.status(400).json({ message: 'Something went wrong when processing the request. Cause unknown.' });
+        }
+        else if (err instanceof Prisma.PrismaClientValidationError) {
+          res.status(400).json({ message: 'Something went wrong when processing the request. Some field(s) seems to be missing or have an incorrect type.' });
+        }
+        else if (err instanceof Prisma.PrismaClientInitializationError) {
+          res.status(500).json({ message: 'Internal server error. Failed to connect to database.' });
+        }
+        else {
+          res.status(500).json({ message: 'Internal server error' });
+        }
+      }
       break;
 
     case 'DELETE':
