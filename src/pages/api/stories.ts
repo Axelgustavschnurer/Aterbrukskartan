25af25575e7d361d00a134f0c1a4bba50f3f2bb9
Leon-Loov <leon.loov@elev.ga.ntig.se>
2023-03-13 14:00:19 +0100

    Add PUT functionality to the Story API

--- src/pages/api/stories.ts
+++ src/pages/api/stories.ts
@@ -39,7 +39,7 @@ export default async function handler(
       else {
         try {
           /**
-           * Returns the `Story` object with the given ID, with the `mapItem` object included, or throws an error if no `Recycle` object with the given ID exists.
+           * Returns the `Story` object with the given ID, with the `mapItem` object included, or throws an error if no `Story` object with the given ID exists.
            */
           const getData: DeepStory = await prisma.story.findFirstOrThrow({
             where: {
@@ -106,14 +106,65 @@ export default async function handler(
       }
       break;
 
+    // On PUT requests, update the `Story` object with the given ID and return it
+    // Throws an error if no ID is specified or no `Story` object with the given ID exists
+    // This is because we only want to update existing objects, new objects should instead be created with POST requests
     case 'PUT':
-      res.status(501).json({ message: 'Not implemented' });
+      try {
+        if (!parseInt(req.query.id as string)) throw new Error('No ID specified');
+
+        const updateData: DeepStoryInput = req.body;
+        /**
+         * Updates the `Story` object with the given ID with the given data, and returns it with the `mapItem` object included.
+         */
+        const updatedData = await prisma.story.update({
+          where: {
+            id: parseInt(req.query.id as string)
+          },
+
+          data: {
+            ...updateData,
+            mapItem: {
+              update: { ...updateData.mapItem }
+            },
+          },
+
+          include: {
+            mapItem: true
+          },
+        });
+
+        res.status(200).json(updatedData);
+      }
+      catch (err: any) {
+        if (err.message === 'No ID specified') {
+          res.status(400).json({ message: 'No ID specified' })
+        }
+        else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+          res.status(400).json({ message: 'Something went wrong when processing the request. The specified ID might not exist in the database.' });
+        }
+        else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+          res.status(400).json({ message: 'Something went wrong when processing the request. Cause unknown.' });
+        }
+        else if (err instanceof Prisma.PrismaClientValidationError) {
+          res.status(400).json({ message: 'Something went wrong when processing the request. Some field(s) seems to be missing or have an incorrect type.' });
+        }
+        else if (err instanceof Prisma.PrismaClientInitializationError) {
+          res.status(500).json({ message: 'Internal server error. Failed to connect to database.' });
+        }
+        else {
+          res.status(500).json({ message: 'Internal server error' });
+        }
+      }
       break;
 
+    // The functionality of DELETE requests is not yet decided, so it is not implemented
+    // We might want to delete `Story` objects, but we might also want to keep them in the database and instead add some kind of 'deleted' tag to them
     case 'DELETE':
       res.status(501).json({ message: 'Not implemented' });
       break;
 
+    // If the request method is not one of the above, return a 405 error
     default:
       res.status(405).json({ message: `Method ${req.method} not allowed` });
       break;
