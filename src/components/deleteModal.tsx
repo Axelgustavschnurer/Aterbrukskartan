7bdb462c79459862672c9017fb062859cd934007
villegust <vilhelm.gustavsson@elev.ga.ntig.se>
2023-03-17 09:52:41 +0100

    Spelling fix

--- src/components/deleteModal.tsx
+++ src/components/deleteModal.tsx
@@ -9,7 +9,7 @@ export default function Modal(props: any) {
     return (
         <div className={`${styles.container} ${modalState ? styles.active : ''}`}>
             <div className={styles.modal}>
-                Vill du värkligen ta bort detta inlägg?
+                Vill du verkligen ta bort detta inlägg?
                 <div className={styles.btnContainer}>
                     <div className={styles.btn} onClick={handleDelete}>
                         <button>Ta bort</button>
