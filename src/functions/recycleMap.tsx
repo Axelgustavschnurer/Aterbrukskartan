a760669c3fa8c230ec28c3edcb549dc4984ba3cc
ahedenskog <albin.hedenskog@elev.ga.ntig.se>
2023-03-23 13:54:06 +0100

    Comments for recycle map

--- src/functions/recycleMap.tsx
+++ src/functions/recycleMap.tsx
@@ -7,6 +7,12 @@ import { DeepRecycle, RecycleFilter } from '@/types'
 import { runActiveFilters } from '@/functions/filters/recycleFilters'
 import { monthArray } from '@/pages/aterbruk'
 
+
+/**
+ * Function to generate a popup for a pin
+ * @param pin Check if the pin is in the current filter
+ * @returns Generates relevant popup for the pin
+ */
 export function recyclePopup(pin: any) {
   return (
     <Popup className='request-popup'>
@@ -18,6 +24,7 @@ export function recyclePopup(pin: any) {
           <b>{pin.projectType}</b> <br />
           {!pin.mapItem.year ? "Projektet har inget planerat startdatum" : pin.mapItem.year && !pin.month ? "Projektet påbörjas år: " + pin.mapItem.year : "Projektet påbörjas: " + monthArray[pin.month - 1] + " " + pin.mapItem.year} <br />
           <>
+            {/* This abomination checks multiple factors of materials being offered or requested and then displays the results */}
             {pin.projectType === "Rivning" && pin.availableMaterials ? <p><b>Erbjuds</b> <br /> {pin.availableMaterials}</p>
               : pin.projectType === "Rivning" && !pin.availableMaterials ? <p><b>Erbjuds</b> <br /> Inget material angivets</p>
                 : pin.projectType === "Nybyggnation" && pin.lookingForMaterials ? <p><b>Sökes</b> <br /> {pin.lookingForMaterials}</p>
@@ -38,6 +45,13 @@ export function recyclePopup(pin: any) {
   )
 }
 
+/**
+ * Function to generate pins for the recycle map
+ * @param mapData Data to be used for the pins
+ * @param currentFilter Current filter to check if the pin is in the current filter
+ * @param searchInput Search input to check if the pin is in the current filter
+ * @returns Generates relevant pins for the map
+ */
 export function recyclePins(mapData: DeepRecycle[], currentFilter: RecycleFilter, searchInput: string | undefined) {
   if (searchInput) {
     currentFilter = { ...currentFilter, searchInput: searchInput }
