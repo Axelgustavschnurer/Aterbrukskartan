556619f2e24f14d1a904e21f85deeb69a7ef3dca
villegust <vilhelm.gustavsson@elev.ga.ntig.se>
2023-03-29 09:40:19 +0200

    Display error screen light or dark depending on your system theme

--- src/errors/404.tsx
+++ src/errors/404.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import styles from "../styles/errors.module.css";
+import { useEffect, useState } from "react";
 
 export default function NotFound() {
+    const [theme, setTheme] = useState("dark");
+
+    useEffect(() => {
+        const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+        const handleThemeChange = (e: any) => {
+            const newTheme = e.matches ? "dark" : "light";
+            setTheme(newTheme);
+        };
+        darkModeQuery.addEventListener("change", handleThemeChange);
+        return () => darkModeQuery.removeEventListener("change", handleThemeChange);
+    }, []);
     return (
-        <div className={styles.container}>
+        <div className={` ${theme === "dark" ? styles.darkMode : styles.lightMode}`}>
             <h1>404 </h1>
             <h2>This page could not be found.</h2>
         </div>
