7ec596e8c04e04c7f0853eb37bbda7935f82902a
villegust <vilhelm.gustavsson@elev.ga.ntig.se>
2023-03-29 09:50:45 +0200

    Added new style to error

--- src/errors/404.tsx
+++ src/errors/404.tsx
@@ -6,16 +6,19 @@ export default function NotFound() {
     const [theme, setTheme] = useState("dark");
 
     useEffect(() => {
+        // Check if the user has set a preference for dark mode
         const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
         const handleThemeChange = (e: any) => {
+            // Update the theme state variable
             const newTheme = e.matches ? "dark" : "light";
             setTheme(newTheme);
         };
+        // Listen for changes to the prefers-color-scheme media query
         darkModeQuery.addEventListener("change", handleThemeChange);
         return () => darkModeQuery.removeEventListener("change", handleThemeChange);
     }, []);
     return (
-        <div className={` ${theme === "dark" ? styles.darkMode : styles.lightMode}`}>
+        <div className={`${styles.container} ${theme === "dark" ? styles.darkMode : styles.lightMode}`}>
             <h1>404 </h1>
             <h2>This page could not be found.</h2>
         </div>
