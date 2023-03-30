eba2240b9a9dd3d40ac39dff6f497437bbda7f5f
Leon-Loov <leon.loov@elev.ga.ntig.se>
2023-03-30 11:24:20 +0200

    Add JSdoc to createOldCsv.ts

--- src/functions/createOldCsv.ts
+++ src/functions/createOldCsv.ts
@@ -1,10 +1,16 @@
 import { oldDataFormat } from "@/pages/api/createOldJSON";
 
-// TODO: Comment this file
-
+/**
+ * Downloads a csv file with the given data and filename
+ * @param csv The csv data to download (actually accepts any string)
+ * @param filename The name of the file to download
+ */
 export function downloadCsv(csv: string, filename: string) {
+  // Create a blob of the data
   const blob = new Blob([csv], { type: 'text/csv' });
   const url = window.URL.createObjectURL(blob);
+
+  // Create a link element, hide it, direct it towards the blob, and then 'click' it programatically
   const a = document.createElement('a');
   a.setAttribute('hidden', '');
   a.setAttribute('href', url);
@@ -14,30 +20,12 @@ export function downloadCsv(csv: string, filename: string) {
   document.body.removeChild(a);
 }
 
+/**
+ * Creates a csv string from the given data
+ * @param data Array of objects to convert to csv
+ * @returns A csv-format string
+ */
 export function createCsv(data: oldDataFormat[]) {
-  // const csvHeaders = [
-  //   "id",
-  //   "name",
-  //   "organisation",
-  //   "year",
-  //   "coordinates",
-  //   "address",
-  //   "postal code",
-  //   "city",
-  //   "category_swedish",
-  //   "category_english",
-  //   "utbildningsprogram",
-  //   "description_swedish",
-  //   "description_english",
-  //   "description_swedish_short",
-  //   "description_english_short",
-  //   "open data",
-  //   "energy stories",
-  //   "reports",
-  //   "videos",
-  //   "pdfcase",
-  // ]
-
   const csvHeaders = Object.keys(data[0]);
 
   let csv: string = csvHeaders.join("|") + "\n";
