bc365e0c203c447c2cd322eb2a33649a858dbb25
villegust <vilhelm.gustavsson@elev.ga.ntig.se>
2023-03-30 09:21:28 +0200

    Light and darkmode changes to 404 error screen

--- src/errors/404.tsx
+++ src/errors/404.tsx
@@ -1,24 +1,9 @@
 import React from "react";
 import styles from "../styles/errors.module.css";
-import { useEffect, useState } from "react";
 
 export default function NotFound() {
-    const [theme, setTheme] = useState("dark");
-
-    useEffect(() => {
-        // Check if the user has set a preference for dark mode
-        const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
-        const handleThemeChange = (e: any) => {
-            // Update the theme state variable
-            const newTheme = e.matches ? "dark" : "light";
-            setTheme(newTheme);
-        };
-        // Listen for changes to the prefers-color-scheme media query
-        darkModeQuery.addEventListener("change", handleThemeChange);
-        return () => darkModeQuery.removeEventListener("change", handleThemeChange);
-    }, []);
     return (
-        <div className={`${styles.container} ${theme === "dark" ? styles.darkMode : styles.lightMode}`}>
+        <div className={styles.container}>
             <h1>404 </h1>
             <h2>This page could not be found.</h2>
         </div>
