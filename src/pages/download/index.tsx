1c0299bd7910bbd5cac093192bf352492860f02e
Leon-Loov <leon.loov@elev.ga.ntig.se>
2023-04-14 10:54:12 +0200

    Add more options to the download page

--- src/pages/download/index.tsx
+++ src/pages/download/index.tsx
@@ -1,31 +1,105 @@
 import React, { useEffect, useState } from "react";
 import { oldDataFormat } from "@/pages/api/createOldJSON";
-import { createCsv, downloadCsv } from "@/functions/createOldCsv";
+import { createOldCsv, downloadCsv, createGenericCsv, createMapItemCsv } from "@/functions/createCsv";
 import styles from "@/styles/downloads.module.css";
 import Image from "next/image";
+import { DeepRecycle, DeepStory } from "@/types";
+import router from "next/router";
+import { websiteKeys } from "@/keys";
 
 export default function DownloadCSV() {
+  // Data from the database
+  // The data is stored in a different format for the old CSV to keep the same format as the one available on the dataportal.
   const [oldStories, setOldStories] = useState({} as { results: oldDataFormat[] })
+  const [storyData, setStoryData] = useState([] as DeepStory[])
+  const [recycleData, setRecycleData] = useState([] as DeepRecycle[])
 
-  const fetchData = async () => {
+  // Whether or not the user has access to the recycle data
+  const [recycleAccess, setRecycleAccess] = useState(false)
+
+  /** Fetches data from the database */
+  const fetchOldData = async () => {
     const res = await fetch('/api/createOldJSON')
     const data = await res.json()
     setOldStories(data)
   }
 
+  /** Fetches data from the database */
+  const fetchStoryData = async () => {
+    const res = await fetch('/api/stories')
+    const data = await res.json()
+    setStoryData(data)
+  }
+
+  /** Fetches data from the database */
+  const fetchRecycleData = async () => {
+    const res = await fetch('/api/recycle')
+    const data = await res.json()
+    setRecycleData(data)
+  }
+
+  // Runs the fetch functions when the page is loaded
   useEffect(() => {
-    fetchData()
+    fetchOldData()
+    fetchStoryData()
+    fetchRecycleData()
   }, [])
 
-  const handleDownload = () => {
-    oldStories?.results?.length ? downloadCsv(createCsv(oldStories.results), "csvTest.csv") : null;
+  // Checks the URL for queries and sets access accordingly
+  // If the query is correct, the user has access to the recycle data and the mapItem data will include things related to the recycle projects.
+  useEffect(() => {
+    // The query is the part of the url after the question mark. It is case sensitive.
+    // For example, if the url is "www.example.com?test=abc", the query is "test=abc", with the key being "test" and the value being "abc".
+    // In order to have multiple queries, they are separated by an ampersand (&).
+    // For example, "www.example.com?test=abc&thing=4" has two queries, "test=abc" and "thing=4".
+    let query = router.query
+
+    query["demoKey"] === websiteKeys["demoKey"] ? setRecycleAccess(true) : setRecycleAccess(false)
+  }, [])
+
+  /** Handles the download of the old CSV */
+  const handleOldDownload = () => {
+    oldStories?.results?.length ? downloadCsv(createOldCsv(oldStories.results), "open_data.csv") : null;
   }
+
+  /** Downloads the story table as a CSV */
+  const handleStoryDownload = () => {
+    storyData?.length ? downloadCsv(createGenericCsv(storyData), "story.csv") : null;
+  }
+
+  /** Downloads the map_item table as a CSV */
+  const handleMapItemDownload = () => {
+    recycleAccess ? downloadCsv(createMapItemCsv([...storyData, ...recycleData]), "map_item.csv") : storyData?.length ? downloadCsv(createMapItemCsv(storyData), "map_item.csv") : null;
+  }
+
+  /** Downloads the recycle table as a CSV */
+  const handleRecycleDownload = () => {
+    recycleData?.length ? downloadCsv(createGenericCsv(recycleData), "recycle.csv") : null;
+  }
+
   return (
     <div className={styles.container}>
-      <h1>Ladda ner CSV</h1>
-      <div id={styles.downloadCsv} onClick={handleDownload}>
+      <h1>Ladda ner gammal stil av CSV för uppladdning till dataportal</h1>
+      <div id={styles.downloadCsv} onClick={handleOldDownload}>
         <Image src="/images/download-button.webp" alt="test" width={1000} height={100} />
       </div>
+      <h1>Ladda ner Stories-data</h1>
+      <div id={styles.downloadCsv} onClick={handleStoryDownload}>
+        <Image src="/images/download-button.webp" alt="test" width={500} height={200} />
+      </div>
+      <h1>Ladda ner mapItem-data (platser, organisationer och år kopplat till annan data)</h1>
+      <div id={styles.downloadCsv} onClick={handleMapItemDownload}>
+        <Image src="/images/download-button.webp" alt="test" width={750} height={150} />
+      </div>
+      {recycleAccess ?
+        <>
+          <h1>Ladda ner Recycle-data</h1>
+          <div id={styles.downloadCsv} onClick={handleRecycleDownload}>
+            <Image src="/images/download-button.webp" alt="test" width={800} height={75} />
+          </div>
+        </>
+        : null
+      }
     </div>
   );
 }
