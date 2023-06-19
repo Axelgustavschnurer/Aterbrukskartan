b3ac609fdd8eba7677bbabe9954746e68a940644
Leon-Loov <leon.loov@elev.ga.ntig.se>
2023-06-19 15:24:22 +0200

    Add loading animation when fetching objects

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -2,6 +2,7 @@ import { MapContainer, Marker, Popup, TileLayer, ZoomControl } from 'react-leafl
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 import React, { useState, useEffect, useMemo } from 'react'
+import Image from 'next/image'
 import MarkerClusterGroup from './markerCluster/index.js'
 import { recyclePins } from '@/functions/recycleMap'
 import { storiesPins } from '@/functions/storiesMap'
@@ -19,7 +20,7 @@ export default function Map({ currentFilter, searchInput, currentMap }: any) {
   const [mapData, setMapData] = useState([])
   const [solarData, setSolarData] = useState([])
 
-
+  const [isLoading, setIsLoading] = useState(false)
 
   // Runs fetchData function on component mount
   useEffect(() => {
@@ -39,7 +40,8 @@ export default function Map({ currentFilter, searchInput, currentMap }: any) {
         setMapData(data)
       }
     }
-    fetchData()
+    setIsLoading(true)
+    fetchData().then(() => setIsLoading(false))
   }, [currentMap])
 
   // Declares map bounds
@@ -49,6 +51,13 @@ export default function Map({ currentFilter, searchInput, currentMap }: any) {
 
   return (
     <>
+      { // Simple loading animation with inline styling
+        // TODO: Remove inline styling
+        isLoading &&
+        <div style={{ position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)", width: 110, height: 110, borderRadius: 10, outline: "#837e7d solid 3px", backgroundColor: "#f1f2f3", display: "flex", alignItems: "center", justifyContent: "center", zIndex: 99 }}>
+          <Image src="/loading.gif" alt="Laddar data" width={100} height={100}></Image>
+        </div>
+      }
       <MapContainer center={[59.858227, 17.632252]} zoom={13} maxZoom={16} minZoom={5} maxBounds={bounds} style={{ height: "100vh", width: "100%" }} zoomControl={false}>
         <ZoomControl position="bottomright" />
         <TileLayer
