99b875a1d23163c72acbdfcac970bba054a471cc
Axel <axel.schnurer@gmail.com>
2023-10-24 15:32:50 +0200

    Final touches

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Head from "next/head";
-import Image from "next/image";
 import Modal from '@/components/redirectModal';
+import Header from "@/components/header/header";
+import TempFooter from "@/components/footer/tempFooter";
 
 export default function Login() {
   const [modal, setModal] = React.useState(false);
@@ -38,13 +39,12 @@ export default function Login() {
         <link rel="icon" type="image/x-icon" href="/stunsicon.ico" />
       </Head>
 
+
+      {/* Header */}
+      <Header />
+
       <div className="layout-main">
 
-        {/* Header */}
-        <div style={{height: '50px', width: '100px', position: 'relative',}}>
-          <Image src="/images/stuns_logo.png" alt="logo" fill style={{objectFit:"contain"}}
- />
-        </div>
         {/* Form */}
 
         <main className="card">
@@ -56,17 +56,14 @@ export default function Login() {
             <label htmlFor="password">Lösenord</label>
             <input type="password" placeholder="lösenord" name="password" id="password" required={true} autoComplete="current-password" />
             <input type="submit" id="save" value="Logga In" className="cta" />
+            
           </form>
         </main>
-
-        {/* Footer */}
-        <a href="https://stuns.se/" target="_blank" rel="noreferrer">
-          STUNS
-        </a>
 
         {/* Redirect modal, after login */}
         <Modal toggle={modal} />
       </div>
+
     </>
   )
 }
