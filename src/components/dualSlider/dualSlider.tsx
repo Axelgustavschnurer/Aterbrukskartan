8a8e33d9640cd3c12b7e9a746555c360278c9229
Axel <axel.schnurer@gmail.com>
2023-12-05 15:23:29 +0100

    Fix firefox support for sliders

--- src/components/dualSlider/dualSlider.tsx
+++ src/components/dualSlider/dualSlider.tsx
@@ -28,8 +28,8 @@ const MultiRangeSlider = ({ min, max, onChange, reset }: any) => {
   // Declares the styling classes and functionality of the range slider. Needs to be given min and max values, as well as onChange function to work.
   return (
     <>
-      {/* The slider thumb that starts at the left*/}
       <div className={styles.slider}>
+        {/* The slider thumb that starts at the left*/}
         <input
           type="range"
           min={min}
@@ -40,7 +40,8 @@ const MultiRangeSlider = ({ min, max, onChange, reset }: any) => {
             setMinVal(value);
             minValRef.current = value;
           }}
-          className={`${styles.thumb} ${styles.thumbLeft}`}
+          style={{right: "0"}}
+          className={`${styles.thumb}`}
         />
         {/* The slider thumb that starts at the right*/}
         <input
@@ -53,18 +54,22 @@ const MultiRangeSlider = ({ min, max, onChange, reset }: any) => {
             setMaxVal(value);
             maxValRef.current = value;
           }}
-          className={`${styles.thumb} ${styles.thumbRight}`}
+          style={{left: "0"}}
+          className={`${styles.thumb}`}
         />
-        {/* The slider track */}
-        <div className={styles.sliderTrack} style={{ background: `linear-gradient(to right, lightGray 0 ${(Math.min(minVal, maxVal) - min) / (max - min) * 100}%, var(--accent) ${(Math.min(minVal, maxVal) - min) / (max - min) * 100}% ${(Math.max(minVal, maxVal) - min) / (max - min) * 100}%, lightGray ${(Math.max(minVal, maxVal) - min) / (max - min) * 100}% 100% )` }} />
-        <div ref={range} className={styles.sliderRange} />
-      </div>
-      <div className={`display-flex justify-content-space-between ${styles.sliderValues}`}>
-        {/* The values displayed at the left and right of the slider. 
+
+        <div>
+          {/* The slider track */}
+          <div className={styles.sliderTrack} style={{ background: `linear-gradient(to right, lightGray 0 ${(Math.min(minVal, maxVal) - min) / (max - min) * 100}%, var(--accent) ${(Math.min(minVal, maxVal) - min) / (max - min) * 100}% ${(Math.max(minVal, maxVal) - min) / (max - min) * 100}%, lightGray ${(Math.max(minVal, maxVal) - min) / (max - min) * 100}% 100% )` }} />
+          <div ref={range} className={styles.sliderRange} />
+          {/* The values displayed at the left and right of the slider. 
                 If the values are less than 12 we assume it's the slider for months and map the results to the monthArray. 
                 Also keeps track of which value is larger for when the thumbs overlap*/}
-        <p className={styles.sliderLeftValue}>{minVal <= 12 && minVal <= maxVal ? monthArray[minVal - 1] : minVal <= 12 && minVal >= maxVal ? monthArray[maxVal - 1] : minVal < maxVal ? minVal : maxVal}</p>
-        <p className={styles.sliderRightValue}>{maxVal <= 12 && maxVal >= minVal ? monthArray[maxVal - 1] : maxVal <= 12 && maxVal <= minVal ? monthArray[minVal - 1] : maxVal > minVal ? maxVal : minVal}</p>
+        </div>
+      </div>
+      <div className={styles.values}>
+        <p>{minVal <= 12 && minVal <= maxVal ? monthArray[minVal - 1] : minVal <= 12 && minVal >= maxVal ? monthArray[maxVal - 1] : minVal < maxVal ? minVal : maxVal}</p>
+        <p>{maxVal <= 12 && maxVal >= minVal ? monthArray[maxVal - 1] : maxVal <= 12 && maxVal <= minVal ? monthArray[minVal - 1] : maxVal > minVal ? maxVal : minVal}</p>
       </div>
     </>
   );
