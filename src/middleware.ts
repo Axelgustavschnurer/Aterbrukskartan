3c2e1be45c5540f9f654147dfef5d328e246b3c7
Leon-Loov <leon.loov@outlook.com>
2023-12-20 14:56:06 +0100

    Change access to /aterbruk
    Require user to be logged in to access /aterbruk pages
    
    Signed-off-by: Leon-Loov <leon.loov@outlook.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,22 +5,22 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const session = await getSession(req, res);
 
-  // Only certain users are allowed access to the recycle admin pages.
+  // Only logged in users are allowed access to the recycle pages.
   if (req.nextUrl.pathname.startsWith("/aterbruk")) {
+    // If the user is not logged in, redirect them to the login page.
+    if (!session.user) {
+      return NextResponse.redirect(new URL("/login", req.nextUrl));
+    }
+
     // To go to any page other than the index recycle page, the user must have permission to add/edit recycle items.
     if (!(req.nextUrl.pathname.endsWith("/aterbruk") || req.nextUrl.pathname.endsWith("/aterbruk/"))) {
-      // If the user is not logged in, redirect them to the login page.
-      if (!session.user) {
-        return NextResponse.redirect(new URL("/login", req.nextUrl));
-      }
-
       if (!(session.user.isRecycler || session.user.isAdmin)) {
         return new NextResponse(JSON.stringify({ message: "You do not have access to this page" }), { status: 403 });
       }
     }
   }
 
-  // Only certain users are allowed access to the stories admin pages. The stories map is located at the root of the website and is thus visible to everyone.
+  // Only certain users are allowed access to the stories admin pages.
   if (req.nextUrl.pathname.startsWith("/stories")) {
     // To go to any page other than the index stories page, the user must have permission to add/edit story items.
     if (!(req.nextUrl.pathname.endsWith("/stories") || req.nextUrl.pathname.endsWith("/stories/"))) {
