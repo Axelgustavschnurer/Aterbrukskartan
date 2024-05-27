d4d79b1c00f25429d3bc3220d4690e05ee3fd032
Axel <axel.schnurer@gmail.com>
2024-05-27 10:54:21 +0200

    Remove unused css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,6 @@ import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 import '../styles/forms.css'
 
-import '../styles/popup.css'
-
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
