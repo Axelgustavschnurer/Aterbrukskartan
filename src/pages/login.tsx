40cceab53019291929fa4fcfd25bd427320d1049
Axel <axel.schnurer@gmail.com>
2024-05-27 11:18:33 +0200

    Remove redirectModal

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import Head from "next/head";
-import Modal from '@/components/redirectModal';
 import Header from "@/components/header/header";
 
 export default function Login() {
-  const [modal, setModal] = React.useState(false);
 
   function handleSubmit(event: any) {
     event.preventDefault()
@@ -15,14 +13,14 @@ export default function Login() {
       password: form.password.value,
     })
 
-    // Try to login, redirect to the home page if successful.
+    // Try to login, redirect to the åtebrukspage
     fetch('/api/login', {
       method: 'POST',
       body: formJSON,
       headers: { 'Content-Type': 'application/json' },
     }).then((res) => {
       if (res.ok) {
-        setModal(true);
+        document.location.href = '/aterbruk'; // TODO: Let user press link if this fails
       } else {
         alert('Login failed.')
       }
@@ -58,9 +56,7 @@ export default function Login() {
             
           </form>
         </main>
-
-        {/* Redirect modal, after login */}
-        <Modal toggle={modal} />
+ 
       </div>
 
     </>
